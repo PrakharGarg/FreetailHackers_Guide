dc3283f243466867a12b6af60b88d25228708208
PrakharGarg <prakhargarg@utexas.edu>
2017-01-03 17:51:52 -0600

    change root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root to: 'home#show'
+  # root to: 'home#show'
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:show]
