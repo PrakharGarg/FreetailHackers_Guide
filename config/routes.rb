da25fc4e6be1b897b384899c55685e2b2d3f275d
PrakharGarg <prakhargarg@utexas.edu>
2017-01-03 19:07:45 -0600

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  # root to: 'home#show'
-  devise_scope :user do
-    root to: "devise/sessions#new"
-  end
+  root to: 'home#show'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:show]
